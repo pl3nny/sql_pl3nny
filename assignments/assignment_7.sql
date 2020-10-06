14c2fa65e92f18f13f39dad0734e2dae12d75dab
pl3nny <pl3nny@gmail.com>
2020-10-06 00:37:43 -0700

    corrected questions

--- assignments/assignment_7.sql
+++ assignments/assignment_7.sql
@@ -12,6 +12,12 @@
 
 -- The following problems are related to these.
 
+SELECT * FROM students -- student_no | student_name | age
+SELECT * FROM student_enrollment -- student_no | course_no
+SELECT * FROM teach -- last_name | course_no
+SELECT * FROM courses -- course_no | course_title | credits
+SELECT * FROM professors -- last_name | department | salary | hire_date
+
 -- QUESTIONS
 
 -- 1. Are the tables student_enrollement and professors direcly related to each other?
@@ -22,14 +28,21 @@
 -- 2. Write a query that shows the student's name, the courses the student is taking and the 
 -- professors that teach that course.
 
-SELECT s.student_name, c.course_title, p.last_name
-FROM students s, courses c, professors p
+SELECT s.student_name, se.course_no, t.last_name
+FROM students s 
+INNER JOIN student_enrollment se
+ON s.student_no = se.student_no
+INNER JOIN teach t
+ON se.course_no = t.course_no
+INNER JOIN professors p
+ON t.last_name = p.last_name
+ORDER BY s.student_name
 
 -- 3. If you execute the query from the prevoius answer, you'll notice the student_name and
 -- the course_no is being repeated. Why is this happening?
 
--- the tables are multiplying with other expanding the number of records
--- 6*5*10 = 300 records
+-- multple professors are teaching some of the same subjects as the other professors which
+-- is causing some of the students to show as repeated rows
 
 -- 4. In question 3 you discovered why there is repeating data. How can we eliminiate this
 -- redundancy? Let's say we only care to see a single professor teaching a course and we
@@ -38,6 +51,8 @@ FROM students s, courses c, professors p
 
 -- HINT: Using the DISTINCT keywork will not help
 
+
+-- before learning you can use INNER JOIN more than once in the same query
 SELECT 
 student_name,
 (SELECT course_title FROM courses WHERE enrolled.course_no = courses.course_no), 
@@ -51,7 +66,25 @@ FROM
 ON students.student_no = enrolled.student_no
 WHERE enrolled.last_name = 'Chong'
 
--- 5. Why aare correlated subqueries slower than non-correlated subqueries and joins?
+-- AFTER LEARNING 
+
+SELECT student_name, course_no, MIN(last_name)
+FROM 
+(
+	SELECT s.student_name, se.course_no, t.last_name
+	FROM students s 
+	INNER JOIN student_enrollment se
+	ON s.student_no = se.student_no
+	INNER JOIN teach t
+	ON se.course_no = t.course_no
+	INNER JOIN professors p
+	ON t.last_name = p.last_name
+	ORDER BY s.student_name, se.course_no
+) a
+GROUP BY student_name, course_no
+ORDER BY student_name, course_no
+
+-- 5. Why are correlated subqueries slower than non-correlated subqueries and joins?
 
 -- correlated subqueries run for every record
 
